bb4e74a96898dddb1edebc79e92910b9ec7d405e
edan <molphone0328@gmail.com>
2025-06-04 15:23:23 +0900

    [feat] 캐릭터 이동, 점수 아이템, 점수 표시 제작

---
+++ Assets/Scripts/ItemSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "2DTopDown/ScoreItem", fileName = "NewItem")]
+
+public class ItemSO : ScriptableObject
+{
+    public int point;
+}
---
+++ Assets/Scripts/ScoreItemObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreItemObject : MonoBehaviour
+{
+    [SerializeField] ItemSO data;
+
+    public int GetPoint()
+    {
+        return data.point;
+    }
+
+}
